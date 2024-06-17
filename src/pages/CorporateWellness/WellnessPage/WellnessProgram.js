ab641da676dd68ccc0f17c883255776a03fa5de9
Mapucheco <159633354+Elmapucheco@users.noreply.github.com>
2024-06-18 00:47:33 +0200

    program

--- src/pages/CorporateWellness/WellnessPage/WellnessProgram.js
+++ src/pages/CorporateWellness/WellnessPage/WellnessProgram.js
@@ -1,31 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { GoDotFill } from "react-icons/go";
 import "./wellnessProgram.css";
+
 const WellnessProgram = () => {
   return (
-    <div className="wellness-program">
-      {/* <h1 className="wellness-title">
-        Yoga for a <span>Healthy Life</span>
-      </h1> */}
-      <h3>Boost your team's well-being with our Employee Yoga Program!</h3>
-      <p className="wellness-description">
+    <div className="program-container">
+      <h1>
+        Boost your team's well-being with our{" "}
+        <span className="span-program">Employee Yoga Program!</span>
+      </h1>
+
+      <p className="program-description">
         Harmony Yoga Institut is excited to offer an initiative designed to help
         your employees to cultivate a focused mind while enjoying the physical
         benefits of yoga.
-        <br />
-        Through our Employee Yoga Program, your team will get access to 4
-        different yoga sessions and choose the one that best fits their current
-        needs.
       </p>
-
-      <div className="program-list">
-        <ul>
-          <li>Raka 1</li>
-          <li>Raka 2</li>
-          <li>Raka 3</li>
-          <li>Raka 4</li>
-        </ul>
-      </div>
-      <div className="wellness-video-container">
+      <div className="program-video-container">
         <iframe
           title="Yoga Video"
           src="https://www.youtube.com/embed/Qr0p2fW1zcQ"
@@ -34,6 +25,34 @@ const WellnessProgram = () => {
           allowFullScreen
         ></iframe>
       </div>
+      <div className="program-list">
+        <ul>
+          <li>
+            <GoDotFill />
+            Yoga for Healthy Back
+          </li>
+          <li>
+            <GoDotFill />
+            Breath and Mindfulness
+          </li>
+          <li>
+            <GoDotFill />
+            Office Chair Yoga
+          </li>
+          <li>
+            <GoDotFill />
+            Yoganidra Relaxation
+          </li>
+        </ul>
+      </div>
+      <p className="program-description">
+        Through our Employee Yoga Program, your team will get access to 4
+        different yoga sessions, available in Czech and English, and choose the
+        one that best fits their current needs.
+      </p>
+      <Link to="/login" className="tologin-button">
+        Sign In to Learn More
+      </Link>
     </div>
   );
 };
