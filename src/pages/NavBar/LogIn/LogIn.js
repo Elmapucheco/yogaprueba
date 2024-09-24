fdb82b314603858d8bb661f53f9834502c1a1b12
Mapucheco <159633354+GabrielAyilef@users.noreply.github.com>
2024-09-24 17:15:07 +0200

    Login

--- src/pages/NavBar/LogIn/LogIn.js
+++ src/pages/NavBar/LogIn/LogIn.js
@@ -1,148 +1,4 @@
-// import React, { useState } from "react";
-// import "./login.css";
-// import { FaUser, FaLock } from "react-icons/fa";
-// import { MdEmail } from "react-icons/md";
-// import { useDarkMode } from "../../../components/Context/DarkMode";
-// import close from "../../../assets/exit.png";
-
-// function LogIn() {
-//   const [action, setAction] = useState("");
-//   const { darkMode } = useDarkMode();
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [showModal, setShowModal] = useState(false);
-
-//   const registerLink = () => {
-//     setAction(" active");
-//   };
-
-//   const loginLink = () => {
-//     setAction("");
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (username && password && (action === " active" ? email : true)) {
-//       setShowModal(true);
-//     }
-//   };
-
-//   const closeModal = () => setShowModal(false);
-
-//   return (
-//     <div className={`login-body ${darkMode ? "dark" : ""}`}>
-//       <div className={`wrapper-login ${action}`}>
-//         <div className="form-box login">
-//           <form onSubmit={handleSubmit}>
-//             <h1>Login</h1>
-//             <div className="input-box">
-//               <input
-//                 type="text"
-//                 placeholder="Username"
-//                 value={username}
-//                 onChange={(e) => setUsername(e.target.value)}
-//                 required
-//               />
-//               <FaUser className="icon" />
-//             </div>
-//             <div className="input-box">
-//               <input
-//                 type="password"
-//                 placeholder="Password"
-//                 value={password}
-//                 onChange={(e) => setPassword(e.target.value)}
-//                 required
-//               />
-//               <FaLock className="icon" />
-//             </div>
-//             <div className="remember-forgot">
-//               <label>
-//                 <input type="checkbox" />
-//                 Remember me
-//               </label>
-//               <a href="#">Forgot password?</a>
-//             </div>
-//             <button type="submit">Login</button>
-//             <div className="register-link">
-//               <p>
-//                 Dont have an account?
-//                 <a href="#" onClick={registerLink}>
-//                   {/* <button type="button" onClick={registerLink}>Register</button> */}
-//                   Register
-//                 </a>
-//               </p>
-//             </div>
-//           </form>
-//         </div>
-//         <div className="form-box register">
-//           <form onSubmit={handleSubmit}>
-//             <h1>Registration</h1>
-//             <div className="input-box">
-//               <input
-//                 type="text"
-//                 placeholder="Username"
-//                 value={username}
-//                 onChange={(e) => setUsername(e.target.value)}
-//                 required
-//               />
-//               <FaUser className="icon" />
-//             </div>
-//             <div className="input-box">
-//               <input
-//                 type="email"
-//                 placeholder="Email"
-//                 value={email}
-//                 onChange={(e) => setEmail(e.target.value)}
-//                 required
-//               />
-//               <MdEmail className="icon" />
-//             </div>
-//             <div className="input-box">
-//               <input
-//                 type="password"
-//                 placeholder="Password"
-//                 value={password}
-//                 onChange={(e) => setPassword(e.target.value)}
-//                 required
-//               />
-//               <FaLock className="icon" />
-//             </div>
-//             <div className="remember-forgot">
-//               <label>
-//                 <input type="checkbox" />I agree to the terms & conditions
-//               </label>
-//             </div>
-//             <button type="submit">Register</button>
-//             <div className="register-link">
-//               <p>
-//                 Already have an account?
-//                 <a href="#" onClick={loginLink}>
-//                   Login
-//                 </a>
-//               </p>
-//             </div>
-//           </form>
-//         </div>
-//       </div>
-//       {showModal && (
-//         <div className="login-modal-content">
-//           <img
-//             className="close"
-//             onClick={closeModal}
-//             src={close}
-//             alt="close message"
-//           />
-//           <h3>This is a demo form for portfolio purposes only.</h3>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default LogIn;
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
 import "./login.css";
 import { FaUser, FaLock } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
@@ -150,54 +6,56 @@ import { useDarkMode } from "../../../components/Context/DarkMode";
 import close from "../../../assets/exit.png";
 
 function LogIn() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
   const [action, setAction] = useState("");
   const { darkMode } = useDarkMode();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const [showModal, setShowModal] = useState(false);
 
-  const registerLink = () => {
+  function registerLink() {
     setAction(" active");
-  };
+  }
 
-  const loginLink = () => {
+  function loginLink() {
     setAction("");
-  };
+  }
 
-  const onSubmit = (data) => {
-    // Puedes acceder a los datos del formulario aquí
-    console.log(data);
-    setShowModal(true);
-  };
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (username && password && (action === " active" ? email : true)) {
+      setShowModal(true);
+    }
+  }
 
-  const closeModal = () => setShowModal(false);
+  function closeModal() {
+    setShowModal(false);
+  }
 
   return (
     <div className={`login-body ${darkMode ? "dark" : ""}`}>
       <div className={`wrapper-login ${action}`}>
-        {/* Login Form */}
         <div className="form-box login">
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit}>
             <h1>Login</h1>
             <div className="input-box">
               <input
                 type="text"
                 placeholder="Username"
-                {...register("username", { required: true })}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
               />
-              {errors.username && <span>Username is required</span>}
               <FaUser className="icon" />
             </div>
             <div className="input-box">
               <input
                 type="password"
                 placeholder="Password"
-                {...register("password", { required: true })}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
               />
-              {errors.password && <span>Password is required</span>}
               <FaLock className="icon" />
             </div>
             <div className="remember-forgot">
@@ -210,7 +68,7 @@ function LogIn() {
             <button type="submit">Login</button>
             <div className="register-link">
               <p>
-                Don't have an account?
+                Dont have an account?
                 <a href="#" onClick={registerLink}>
                   Register
                 </a>
@@ -218,36 +76,37 @@ function LogIn() {
             </div>
           </form>
         </div>
-
-        {/* Registration Form */}
         <div className="form-box register">
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit}>
             <h1>Registration</h1>
             <div className="input-box">
               <input
                 type="text"
                 placeholder="Username"
-                {...register("username", { required: true })}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
               />
-              {errors.username && <span>Username is required</span>}
               <FaUser className="icon" />
             </div>
             <div className="input-box">
               <input
                 type="email"
                 placeholder="Email"
-                {...register("email", { required: true })}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
               />
-              {errors.email && <span>Email is required</span>}
               <MdEmail className="icon" />
             </div>
             <div className="input-box">
               <input
                 type="password"
                 placeholder="Password"
-                {...register("password", { required: true })}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
               />
-              {errors.password && <span>Password is required</span>}
               <FaLock className="icon" />
             </div>
             <div className="remember-forgot">
@@ -267,8 +126,6 @@ function LogIn() {
           </form>
         </div>
       </div>
-
-      {/* Modal */}
       {showModal && (
         <div className="login-modal-content">
           <img
@@ -285,3 +142,156 @@ function LogIn() {
 }
 
 export default LogIn;
+// import React, { useState } from "react";
+// import { useForm } from "react-hook-form";
+// import "./login.css";
+// import { FaUser, FaLock } from "react-icons/fa";
+// import { MdEmail } from "react-icons/md";
+// import { useDarkMode } from "../../../components/Context/DarkMode";
+// import close from "../../../assets/exit.png";
+
+// function LogIn() {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm();
+//   const [action, setAction] = useState("");
+//   const { darkMode } = useDarkMode();
+//   const [showModal, setShowModal] = useState(false);
+
+//   const registerLink = () => setAction(" active");
+//   const loginLink = () => setAction("");
+
+//   const onLoginSubmit = (data) => {
+//     console.log("Datos de Login:", data);
+//     setShowModal(true);
+//   };
+
+//   const onRegisterSubmit = (data) => {
+//     console.log("Datos de Registro:", data);
+//     setShowModal(true);
+//   };
+
+//   const closeModal = () => setShowModal(false);
+
+//   return (
+//     <div className={`login-body ${darkMode ? "dark" : ""}`}>
+//       <div className={`wrapper-login ${action}`}>
+//         {/* Formulario de Login */}
+//         <div className="form-box login">
+//           <form onSubmit={handleSubmit(onLoginSubmit)}>
+//             <h1>Login</h1>
+//             <div className="input-box">
+//               <input
+//                 type="text"
+//                 placeholder="Username"
+//                 {...register("loginUsername", { required: true })} // Cambiar nombre para evitar conflictos
+//               />
+//               {errors.loginUsername && (
+//                 <span>El nombre de usuario es obligatorio</span>
+//               )}
+//               <FaUser className="icon" />
+//             </div>
+//             <div className="input-box">
+//               <input
+//                 type="password"
+//                 placeholder="Password"
+//                 {...register("loginPassword", { required: true })} // Cambiar nombre para evitar conflictos
+//               />
+//               {errors.loginPassword && (
+//                 <span>La contraseña es obligatoria</span>
+//               )}
+//               <FaLock className="icon" />
+//             </div>
+//             <div className="remember-forgot">
+//               <label>
+//                 <input type="checkbox" />
+//                 Recordarme
+//               </label>
+//               <a href="#">¿Olvidaste tu contraseña?</a>
+//             </div>
+//             <button type="submit">Login</button>
+//             <div className="register-link">
+//               <p>
+//                 ¿No tienes una cuenta?
+//                 <a href="#" onClick={registerLink}>
+//                   Registrar
+//                 </a>
+//               </p>
+//             </div>
+//           </form>
+//         </div>
+
+//         {/* Formulario de Registro */}
+//         <div className="form-box register">
+//           <form onSubmit={handleSubmit(onRegisterSubmit)}>
+//             <h1>Registro</h1>
+//             <div className="input-box">
+//               <input
+//                 type="text"
+//                 placeholder="Username"
+//                 {...register("registerUsername", { required: true })} // Cambiar nombre para evitar conflictos
+//               />
+//               {errors.registerUsername && (
+//                 <span>El nombre de usuario es obligatorio</span>
+//               )}
+//               <FaUser className="icon" />
+//             </div>
+//             <div className="input-box">
+//               <input
+//                 type="email"
+//                 placeholder="Email"
+//                 {...register("email", { required: true })}
+//               />
+//               {errors.email && <span>El email es obligatorio</span>}
+//               <MdEmail className="icon" />
+//             </div>
+//             <div className="input-box">
+//               <input
+//                 type="password"
+//                 placeholder="Password"
+//                 {...register("registerPassword", { required: true })} // Cambiar nombre para evitar conflictos
+//               />
+//               {errors.registerPassword && (
+//                 <span>La contraseña es obligatoria</span>
+//               )}
+//               <FaLock className="icon" />
+//             </div>
+//             <div className="remember-forgot">
+//               <label>
+//                 <input type="checkbox" /> Acepto los términos y condiciones
+//               </label>
+//             </div>
+//             <button type="submit">Registrar</button>
+//             <div className="register-link">
+//               <p>
+//                 ¿Ya tienes una cuenta?
+//                 <a href="#" onClick={loginLink}>
+//                   Login
+//                 </a>
+//               </p>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+
+//       {/* Modal */}
+//       {showModal && (
+//         <div className="login-modal-content">
+//           <img
+//             className="close"
+//             onClick={closeModal}
+//             src={close}
+//             alt="cerrar mensaje"
+//           />
+//           <h3>
+//             Este es un formulario de demostración solo para fines de portafolio.
+//           </h3>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default LogIn;
